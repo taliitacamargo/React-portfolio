39cbd78dea76360760a80e39a866135034add9fd
Tali <taliitacamargo@icloud.com>
2022-02-11 13:58:23 -0500

    fixing background repeat

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import TheContainer from './Components/TheContainer';
 import '../src/App.css';
-import backgroundVideo from '../src/Components/Content/knight.mp4';
+// import backgroundVideo from '../src/Components/Content/knight.mp4';
 
 function App () {
     return (
 <div>
-
-<TheContainer/>;
-
+<TheContainer/>
 </div>
     )
 }
