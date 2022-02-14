9160671cb4f5672b3e7c43c74108abd4fc9c7317
Tali <taliitacamargo@icloud.com>
2022-02-14 13:02:32 -0500

    adding message me box

--- portfolio/src/Components/Content/Contact.js
+++ portfolio/src/Components/Content/Contact.js
@@ -1,44 +1,97 @@
 import React from 'react';
-import resume from '../assets/resume.pdf'
+import resume from '../assets/resume.pdf';
+import { useForm, ValidationError } from '@formspree/react';
 
 
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaUserTie } from "react-icons/fa";
-import { FaEnvelopeOpen} from "react-icons/fa";
+import { FaEnvelopeOpen } from "react-icons/fa";
 
 
 export default function Contact() {
-    return(
-    <div className = "container">
-        <h2>Let's Connect!</h2>
+    const [state, handleSubmit] = useForm("xvolazrb");
+    if (state.succeeded) {
         
-        <nav className="nav" id="contact-info">
-            <ul className="links">
-            <li>
-                    <a style={{"fontSize":"45px"}}  href="mailto:taliitacamargo@icloud.com"><FaEnvelopeOpen className= "grow"/></a>
-                    <small>Email</small>
-                    
-                </li>
-                <li>
-                    <a  style={{"fontSize":"45px"}}  href = {resume}><FaUserTie className= "grow"/></a>
-                    <small>Resume</small>
-                    
-                </li>
-                <li>
-                    <a  style={{"fontSize":"45px"}} href="https://www.linkedin.com/in/talita-camargo-03a40a52/"><FaLinkedin className= "grow"/></a>
-                    <small>LinkedIn</small>
-                    
-                </li>
-                <li>
-                    <a style={{"fontSize":"45px"}}  href="https://github.com/taliitacamargo"><FaGithubSquare className= "grow"/></a>
-          
-                    <small>GitHub</small>
-                   
-                </li>
+        return (
+        <div className = "container">
+            <h2>Your message has been sent!</h2>
+        </div>
+        )
+    }
+    return (
+        <div className="container">
+            
+                <h1>Let's Connect</h1>
+
+            <div className= "form">
+                
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email"/>
+                 
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder ="johndoe@example.com"
+                />
               
-            </ul>
-        </nav>
-    </div>
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+              
+                <textarea
+                    id="message"
+                    name="message"
+                    placeholder ="Your Message Here"
+                />
+
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
+                
+                <button type="submit" disabled={state.submitting}>
+                    Submit
+                </button>
+            </form>
+            </div>
+        <div className= "hr">
+        <hr/>
+        </div>
+
+            <div>
+
+                <nav className="nav" id="contact-info">
+                    <ul className="links">
+                        <li>
+                            <a style={{ "fontSize": "45px" }} href="mailto:taliitacamargo@icloud.com"><FaEnvelopeOpen className="grow" /></a>
+                            <small>Email</small>
+
+                        </li>
+                        <li>
+                            <a style={{ "fontSize": "45px" }} href={resume}><FaUserTie className="grow" /></a>
+                            <small>Resume</small>
+
+                        </li>
+                        <li>
+                            <a style={{ "fontSize": "45px" }} href="https://www.linkedin.com/in/talita-camargo-03a40a52/"><FaLinkedin className="grow" /></a>
+                            <small>LinkedIn</small>
+
+                        </li>
+                        <li>
+                            <a style={{ "fontSize": "45px" }} href="https://github.com/taliitacamargo"><FaGithubSquare className="grow" /></a>
+
+                            <small>GitHub</small>
+
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
     );
 }
